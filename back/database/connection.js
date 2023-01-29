98c0f2a9e5e5778b3870e7f371045436d40132d4
braalexdeveloper <braalexdeveloper@gmail.com>
2023-01-28 21:02:55 -0500

    header

--- back/database/connection.js
+++ back/database/connection.js
@@ -4,7 +4,7 @@ const connection=async()=>{
     try {
         await mongoose.connect("mongodb://mongo:JFmnLfkeKNq5kF724Hp7@containers-us-west-146.railway.app:6495");
         console.log("Conectado correctamente!!");
-        //mongodb://127.0.0.1/mi_redsocial    
+        //mongodb://127.0.0.1/mi_redsocial    mongodb://mongo:JFmnLfkeKNq5kF724Hp7@containers-us-west-146.railway.app:6495
     } catch (error) {
         console.log(error);
         throw new Error("No se ha podido conectar a la base de datos");
--- back/index.js
+++ back/index.js
@@ -18,11 +18,21 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 //config rutas
 /*const userRouter=require("./routes/user");
 const followRouter=require("./routes/follow");
 const publicationRouter=require("./routes/publication");*/
-
+app.use(express.json());
 const rutass=require("./routes/index");
 
 /*app.use('/api/user',userRouter);
